2f758b2d1b23a75880ee9aa2b430d5f0cd9a75a5
Michele Bertasi <michele.bertasi@gmail.com>
2021-02-27 12:09:30 +0100

    Add separate ComputeOutdated.
    This will be used in a `check` subcommand.

--- internal/backup/backup.go
+++ internal/backup/backup.go
@@ -13,34 +13,59 @@ import (
 	"github.com/mbrt/backsched/internal/exec"
 )
 
+// ComputeOutdated returns the list of outdated backups.
+func ComputeOutdated(ctx context.Context, cfg config.Config, env Env) ([]Info, error) {
+	var res []Info
+	state := loadState(env.Sio)
+
+	for _, bc := range cfg.Backups {
+		clog := log.With().Str("backup", bc.Name).Logger()
+		t, ok := state.LastBackupOf(bc.Name)
+		since := env.Clock.Now().Sub(t)
+		if ok && since < time.Duration(bc.Interval) {
+			clog.Info().Msgf("Skipping because last backup was at: %v", t)
+			continue
+		}
+		res = append(res, Info{
+			Since:  since,
+			Backup: bc,
+		})
+	}
+
+	return res, nil
+}
+
 // Run runs the given backup configuration.
 func Run(ctx context.Context, cfg config.Config, env Env, dry bool) error {
-	state := loadState(env.Sio)
+	backups, err := ComputeOutdated(ctx, cfg, env)
+	if err != nil {
+		return err
+	}
+
+	state := config.State{}
 	if dry {
 		// Avoid running anything.
 		env.Runner = dryRunner{}
 	} else {
-		// Make sure we save the state at the end.
+		// Make sure we update the state at the end.
+		state = loadState(env.Sio)
 		defer saveState(env.Sio, state)
 	}
 
-	for _, bc := range cfg.Backups {
-		clog := log.With().Str("backup", bc.Name).Logger()
-		if t, ok := state.LastBackupOf(bc.Name); ok && env.Clock.Now().Sub(t) < time.Duration(bc.Interval) {
-			clog.Info().Msgf("Skipping because last backup was at: %v", t)
-			continue
-		}
+	for _, bc := range backups {
+		name := bc.Backup.Name
+		clog := log.With().Str("backup", name).Logger()
 
-		b := newExecutorFromConfig(bc, env)
+		b := newExecutorFromConfig(bc.Backup, env)
 		if err := b.CanExecute(ctx); err != nil {
 			clog.Info().Msgf("Skipping because: %v", err)
 			continue
 		}
 		clog.Info().Msg("Executing")
 		if err := b.Run(ctx); err != nil {
-			return fmt.Errorf("executing backup %q: %w", bc.Name, err)
+			return fmt.Errorf("executing backup %q: %w", name, err)
 		}
-		state[bc.Name] = env.Clock.Now()
+		state[name] = env.Clock.Now()
 	}
 
 	return nil
@@ -59,6 +84,34 @@ type Env struct {
 type StateIOer interface {
 	Save(buf []byte) error
 	Load() ([]byte, error)
+}
+
+// Info represents the state of a backup.
+type Info struct {
+	// Since contains how long ago the backup was performed.
+	// Zero is a special value meaning "never".
+	Since  time.Duration
+	Backup config.Backup
+}
+
+func (i Info) String() string {
+	if i.Since == 0 {
+		return fmt.Sprintf("%s: never done before", i.Backup.Name)
+	}
+	return fmt.Sprintf("%s: %s ago", i.Backup.Name, fmtDuration(i.Since))
+}
+
+func fmtDuration(d time.Duration) string {
+	if d < time.Minute {
+		return "less than a minute"
+	}
+	if d < time.Hour {
+		return d.Truncate(time.Minute).String()
+	}
+	if d < time.Hour*24 {
+		return d.Truncate(time.Hour).String()
+	}
+	return fmt.Sprintf("%d days", int(d/time.Hour/24))
 }
 
 func loadState(sio StateIOer) config.State {
